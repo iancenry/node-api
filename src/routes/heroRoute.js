6f0e9dfe6e594e42986a22473146b279deda9124
Ian Cenry <iancenry@gmail.com>
2023-10-30 22:31:37 +0300

    feat: create user - POST route

--- src/routes/heroRoute.js
+++ src/routes/heroRoute.js
@@ -1,11 +1,28 @@
+import { once } from 'node:events';
+import Hero from '../entities/hero.js';
+import { DEFAULT_HEADER } from '../util/util.js';
+
 const routes = ({ heroService }) => ({
   '/heroes:get': async (req, res) => {
     res.write('GET');
     res.end();
   },
   '/heroes:post': async (req, res) => {
-    res.write('POST');
-    res.end();
+    // get item once the request sends some data
+    const data = await once(req, 'data');
+    const item = JSON.parse(data);
+    const hero = new Hero(item);
+
+    const id = hero.id;
+    res.writeHead(201, DEFAULT_HEADER);
+    res.write(
+      JSON.stringify({
+        id,
+        success: 'User created with successs!!',
+      })
+    );
+
+    return res.end();
   },
 });
 
